f9d52e4dad8ba5a8cb3cc93859d551bc34b8a2e3
mubashir369 <muhammedmubashir3693@gmail.com>
2022-09-26 20:17:38 +0530

    filter api

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,26 +1,46 @@
 import "./featuredProperties.css";
-
+import useFetch from "../../hooks/useFetch";
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch(
-    "http://localhost:5000/api/hotels/countByType"
+    "http://localhost:5000/api/hotels?featured=true&limit=4"
   );
+  console.log("daaaaaaaa", data);
+  if (!data[0]) {
+    return "Loading";
+  }
   return (
     <div className="fp">
-      <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-          className="fpImg"
-        />
-        <span className="fpName">Aparthotel Stare Miasto</span>
-        <span className="fpCity">Madrid</span>
-        <span className="fpPrice">Starting from $120</span>
-        <div className="fpRating">
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-      <div className="fpItem">
+      {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {data.map((item) => {
+            return(
+<div className="fpItem" key={item._id}>
+            
+            <img
+              src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+              alt=""
+              className="fpImg"
+            />
+            <span className="fpName">{item.name}</span>
+            <span className="fpCity">{item.city}</span>
+            <span className="fpPrice">
+              Starting from ${item.cheapestPrice}
+            </span>
+            {item.rating && (
+              <div className="fpRating">
+                <button>{item.rating}</button>
+                <span>Excellent</span>
+              </div>
+            )}
+          </div>
+            )
+            
+          })}
+        </>
+      )}
+      {/* <div className="fpItem">
         <img
           src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
           alt=""
@@ -61,7 +81,7 @@ const FeaturedProperties = () => {
           <button>8.9</button>
           <span>Excellent</span>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
