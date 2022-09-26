8e81ce8a31e20cdc33feab09ec2bb09d78e68884
mubashir369 <muhammedmubashir3693@gmail.com>
2022-09-26 20:49:42 +0530

    hotel single page

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -15,28 +15,26 @@ const FeaturedProperties = () => {
       ) : (
         <>
           {data.map((item) => {
-            return(
-<div className="fpItem" key={item._id}>
-            
-            <img
-              src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-              alt=""
-              className="fpImg"
-            />
-            <span className="fpName">{item.name}</span>
-            <span className="fpCity">{item.city}</span>
-            <span className="fpPrice">
-              Starting from ${item.cheapestPrice}
-            </span>
-            {item.rating && (
-              <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
+            return (
+              <div className="fpItem" key={item._id}>
+                <img
+                  src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+                  alt=""
+                  className="fpImg"
+                />
+                <span className="fpName">{item.name}</span>
+                <span className="fpCity">{item.city}</span>
+                <span className="fpPrice">
+                  Starting from ${item.cheapestPrice}
+                </span>
+                {item.rating && (
+                  <div className="fpRating">
+                    <button>{item.rating}</button>
+                    <span>Excellent</span>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-            )
-            
+            );
           })}
         </>
       )}
